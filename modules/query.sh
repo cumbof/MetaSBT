2f2702cdf29a3ebbe4334b654ee8350f569a1012
fabio-cumbo <fabio.cumbo@gmail.com>
2022-05-10 21:22:02 -0400

    Updating query module

--- modules/query.sh
+++ modules/query.sh
@@ -16,12 +16,14 @@ source ${QUERY_DIR}/utils.sh
 # Do not expand queries by default
 # If true, keep querying trees at higher taxonomic levels
 EXPAND_QUERY=false
+# Theta threshold
+THRESHOLD=0.7
 
 # Parse input arguments
 for ARG in "$@"; do
     case "$ARG" in
         --expand)
-            # Expand the input query on all the taxonomic levels
+            # Expand the input query to the higher taxonomic levels
             EXPAND_QUERY=true
             ;;
         -h|--help)
@@ -45,13 +47,23 @@ for ARG in "$@"; do
             printf "%s\n" "$(cat ${QUERY_DIR}/../LICENSE)"
             exit 0
             ;;
-        --output=*)
-            # Output file path with matches
-            OUTPUT="${ARG#*=}"
+        --output-dir=*)
+            # Output folder
+            OUTPUTDIR="${ARG#*=}"
             # Define helper
-            if [[ "${OUTPUT}" =~ "?" ]]; then
-                printf "query helper: --output=file\n\n"
-                printf "\tThis is the output file with matches.\n\n"
+            if [[ "${OUTPUTDIR}" =~ "?" ]]; then
+                printf "query helper: --output-dir=file\n\n"
+                printf "\tThis is the output folder with queries results.\n\n"
+                exit 0
+            fi
+            ;;
+        --output-prefix=*)
+            # Output folder
+            OUTPUTPREFIX="${ARG#*=}"
+            # Define helper
+            if [[ "${OUTPUTPREFIX}" =~ "?" ]]; then
+                printf "query helper: --output-prefix=file\n\n"
+                printf "\tPrefix of the output files with query matches.\n\n"
                 exit 0
             fi
             ;;
@@ -59,6 +71,18 @@ for ARG in "$@"; do
             # Check for external software dependencies and python modules
             check_dependencies
             exit $?
+            ;;
+        --threshold=*)
+            # Theta threshold
+            THRESHOLD="${ARG#*=}"
+            # Define helper
+            if [[ "${THRESHOLD}" =~ "?" ]]; then
+                printf "query helper: --threshold=number\n\n"
+                printf "\tFraction of query kmers that must be present in a leaf to be considered a match.\n"
+                printf "\tThis must be between 0 and 1.\n"
+                printf "\tDefault: 0.7\n\n"
+                exit 0
+            fi
             ;;
         --tree=*)
             # Tree definition file path
@@ -82,4 +106,98 @@ for ARG in "$@"; do
     esac
 done
 
-# TODO
+printf "query version %s (%s)\n\n" "$VERSION" "$DATE"
+PIPELINE_START_TIME="$(date +%s.%3N)"
+
+check_dependencies
+if [[ "$?" -gt "0" ]]; then
+    exit 1
+fi
+
+printf "Query:\n"
+printf "\t%s\n\n" "${INPUT}"
+
+# Create the output directory if it does not exist
+if [[ ! -d ${OUTPUTDIR} ]]; then
+    mkdir -p ${OUTPUTDIR}
+fi
+
+# Take track of the best matches
+# Closest taxa
+LINEAGE=()
+# Scores of the closest taxa
+SCORES=()
+# Closest genome
+CLOSEST_GENOME=""
+# Score of the closest genome
+CLOSEST_GENOME_SCORE=0
+
+printf "Querying trees:\n"
+# Start querying the tree
+while [[ -f ${TREE} ]]; do
+    IDXDIR="$(dirname $TREE)"
+    LEVELDIR="$(dirname $IDXDIR)"
+    LEVEL="$(basename $LEVELDIR)"
+    # Query the sequence bloom tree
+    printf "\t%s\n" "${TREE}"
+    howdesbt query --tree=${TREE} \
+                   --threshold=${THRESHOLD} \
+                   --adjust \
+                   --sort ${INPUT} \
+                   > ${OUTPUTDIR}/${OUTPUTPREFIX}__${LEVEL}__matches.txt
+    # Get best match
+    BEST=$(grep -E "^[[:alnum:]]" prova.txt | head -n 1)
+    MATCH=$(echo "$BEST" | cut -d' ' -f1)
+    SCORE=$(echo "$BEST" | cut -d' ' -f5)
+
+    if [[ "$LEVEL" = s__* ]]; then
+        # There is nothing after the species tree
+        CLOSEST_GENOME=$MATCH
+        CLOSEST_GENOME_SCORE=$SCORE
+        break
+    else
+        # Update the lineage
+        LINEAGE+=("$MATCH")
+        SCORES+=("$SCORE")
+
+        # Keep querying if --expand
+        TREE=${LEVELDIR}/${MATCH}/index/index.detbrief.sbt
+        if [[ "$MATCH" = s__* ]]; then
+            TREE=${LEVELDIR}/${MATCH}/index/howde_index/index.detbrief.sbt
+        fi
+
+        # Stop querying trees
+        if [[ "${EXPAND_QUERY}" = false ]]; then
+            break
+        fi
+    fi
+done
+
+# Reconstruct the lineage
+if [ ${#LINEAGE[@]} -eq 0 ]; then
+    # Print closest lineage
+    printf "Closest lineage:\n"
+    CLOSEST_LINEAGE=$(printf "|%s" "${LINEAGE[@]}")
+    CLOSEST_LINEAGE=${CLOSEST_LINEAGE:1}
+    printf "\t%s\n\n" "${CLOSEST_LINEAGE}"
+
+    # Print scores
+    printf "Score:\n"
+    for i in "${!LINEAGE[@]}"; do
+        printf "\t%s\t%s\n" "${LINEAGE[i]}" "${SCORES[i]}"
+    done
+    printf "\n"
+fi
+
+# Show the closest genome in case the input is a species tree or the query is expanded
+if [ ! -z "${CLOSEST_GENOME}" ]; then
+    # Print the closest genome and its score
+    printf "Closest genome:\n"
+    printf "\t%s\t%s\n" "${CLOSEST_GENOME}" "${CLOSEST_GENOME_SCORE}"
+fi
+
+PIPELINE_END_TIME="$(date +%s.%3N)"
+PIPELINE_ELAPSED="$(bc <<< "${PIPELINE_END_TIME}-${PIPELINE_START_TIME}")"
+printf "\nTotal elapsed time: %s\n\n" "$(displaytime ${PIPELINE_ELAPSED})"
+
+exit 0
