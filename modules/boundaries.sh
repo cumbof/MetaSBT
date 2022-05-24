798759c8a508989b3f57265946828b3ce4192ecd
fabio-cumbo <fabio.cumbo@gmail.com>
2022-05-24 18:19:46 -0400

    Updating boundaries module

--- modules/boundaries.sh
+++ modules/boundaries.sh
@@ -4,7 +4,7 @@
 #author         :Fabio Cumbo (fabio.cumbo@gmail.com)
 #=====================================================================================================
 
-DATE="May 18, 2022"
+DATE="May 24, 2022"
 VERSION="0.1.0"
 
 # Define script directory
@@ -13,29 +13,91 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # Import utility functions
 source ${SCRIPT_DIR}/utils.sh
 
+# Remove temporary data at the end of the pipeline
+CLEANUP=false
+
 # Parse input arguments
 for ARG in "$@"; do
     case "$ARG" in
+        --cleanup)
+            # Remove temporary data at the end of the pipeline
+            CLEANUP=true
+            ;;
+        --db-dir=*)
+            # Database directory
+            DBDIR="${ARG#*=}"
+            # Define helper
+            if [[ "${DBDIR}" =~ "?" ]]; then
+                printf "boundaries helper: --db-dir=directory\n\n"
+                printf "\tThis is the database directory with the taxonomically organised sequence bloom trees.\n\n"
+                exit 0
+            fi
+            # Reconstruct the full path
+            DBDIR="$( cd "$( dirname "${DBDIR}" )" &> /dev/null && pwd )"/"$( basename $DBDIR )"
+            # Trim the last slash out of the path
+            DBDIR="${DBDIR%/}"
+            ;;
         -h|--help)
             # Print extended help
-            UPDATE_HELP=true
+            BOUNDARIES_HELP=true
             source ${SCRIPT_DIR}/../HELP
             exit 0
             ;;
-        --license)
-            # Print license
-            printf "%s\n" "$(cat ${SCRIPT_DIR}/../LICENSE)"
-            exit 0
+        --nproc=*)
+            # Max nproc for all parallel instructions
+            NPROC="${ARG#*=}"
+            # Define helper
+            if [[ "${NPROC}" =~ "?" ]]; then
+                printf "boundaries helper: --nproc=num\n\n"
+                printf "\tThis argument refers to the number of processors used for parallelizing the pipeline when possible.\n"
+                printf "\tDefault: --nproc=1\n\n"
+                exit 0
+            fi
+            # Check whether --proc is an integer
+            if [[ ! $NPROC =~ ^[0-9]+$ ]] || [[ "$NPROC" -eq "0" ]]; then
+                printf "Argument --nproc must be a positive integer greater than 0\n"
+                exit 1
+            fi
             ;;
-        --resolve-dependencies)
-            # Check for external software dependencies and python modules
-            check_dependencies
-            exit $?
+        --output=*)
+            # Output file
+            OUTPUTFILE="${ARG#*=}"
+            # Define helper
+            if [[ "${OUTPUTFILE}" =~ "?" ]]; then
+                printf "boundaries helper: --output=file\n\n"
+                printf "\tOutput file with kmer boundaries for each of the taxonomic labels in the database.\n\n"
+                exit 0
+            fi
             ;;
-        -v|--version)
-            # Print pipeline version
-            printf "boundaries version %s (%s)\n" "$VERSION" "$DATE"
-            exit 0
+        --tmp-dir=*)
+            # Temporary folder
+            TMPDIR="${ARG#*=}"
+            # Define helper
+            if [[ "${TMPDIR}" =~ "?" ]]; then
+                printf "boundaries helper: --tmp-dir=directory\n\n"
+                printf "\tPath to the folder for storing temporary data.\n\n"
+                exit 0
+            fi
+            # Reconstruct the full path
+            TMPDIR="$( cd "$( dirname "${TMPDIR}" )" &> /dev/null && pwd )"/"$( basename $TMPDIR )"
+            # Trim the last slash out of the path
+            TMPDIR="${TMPDIR%/}"
+            ;;
+        --xargs-nproc=*)
+            # Max number of independent runs of kmtricks
+            XARGS_NPROC="${ARG#*=}"
+            # Define helper
+            if [[ "${XARGS_NPROC}" =~ "?" ]]; then
+                printf "boundaries helper: --xargs-nproc=num\n\n"
+                printf "\tThis refers to the number of xargs processes used for launching independent runs of kmtricks.\n"
+                printf "\tDefault: --xargs-nproc=1\n\n"
+                exit 0
+            fi
+            # Check whether --xargs-nproc is an integer
+            if [[ ! $XARGS_NPROC =~ ^[0-9]+$ ]] || [[ "$XARGS_NPROC" -eq "0" ]]; then
+                printf "Argument --xargs-nproc must be a positive integer greater than 0\n"
+                exit 1
+            fi
             ;;
         *)
             printf "boundaries: invalid option -- %s\n" "$ARG"
@@ -47,12 +109,22 @@ done
 printf "boundaries version %s (%s)\n\n" "$VERSION" "$DATE"
 PIPELINE_START_TIME="$(date +%s.%3N)"
 
-check_dependencies
+check_dependencies false
 if [[ "$?" -gt "0" ]]; then
     exit 1
 fi
 
+# TODO
 
+# Cleanup temporary data
+if ${CLEANUP}; then
+    # Remove tmp folder
+    if [[ -d $TMPDIR ]]; then
+        printf "Cleaning up temporary folder:\n"
+        printf "\t%s\n" "${TMPDIR}"
+        rm -rf ${TMPDIR}
+    fi
+fi
 
 PIPELINE_END_TIME="$(date +%s.%3N)"
 PIPELINE_ELAPSED="$(bc <<< "${PIPELINE_END_TIME}-${PIPELINE_START_TIME}")"
