c7255828b6c787916ee6f276048c58b960f82ea6
fabio-cumbo <fabio.cumbo@gmail.com>
2022-05-25 20:21:22 -0400

    Updating update module

--- modules/update.sh
+++ modules/update.sh
@@ -4,7 +4,7 @@
 #author         :Fabio Cumbo (fabio.cumbo@gmail.com)
 #===================================================
 
-DATE="May 24, 2022"
+DATE="May 25, 2022"
 VERSION="0.1.0"
 
 # Define script directory
@@ -134,6 +134,16 @@ for ARG in "$@"; do
                 exit 0
             fi
             ;;
+        --kingdom=*)
+            # Consider genomes whose lineage belong to a specific kingdom only
+            KINGDOM="${ARG#*=}"
+            # Define helper
+            if [[ "${KINGDOM}" =~ "?" ]]; then
+                printf "update helper: --kingdom=value\n\n"
+                printf "\tSelect the kingdom that will be updated.\n\n"
+                exit 0
+            fi
+            ;;
         --kmer-len=*)
             # Length of the kmers
             KMER_LEN="${ARG#*=}"
@@ -218,11 +228,20 @@ PIPELINE_START_TIME="$(date +%s.%3N)"
 
 check_dependencies false
 if [[ "$?" -gt "0" ]]; then
+    printf "Unsatisfied software dependencies!\n\n"
+    printf "Please run the following command for a list of required external software dependencies:\n\n"
+    printf "\t$ meta-index --resolve-dependencies\n\n"
+    
     exit 1
 fi
 
 # Create temporary folder
 mkdir -p $TMPDIR
+
+# Count how many genomes in input
+HOW_MANY=$(cat ${INLIST} | wc -l)
+printf "Processing %s genomes\n" "${HOW_MANY}"
+printf "\t%s\n\n" "${INLIST}"
 
 # Input genomes must be quality-controlled before being added to the database
 if [[ "${CHECKM_COMPLETENESS}" -gt "0" ]] && [[ "${CHECKM_CONTAMINATION}" -lt "100" ]]; then
@@ -232,7 +251,8 @@ if [[ "${CHECKM_COMPLETENESS}" -gt "0" ]] && [[ "${CHECKM_CONTAMINATION}" -lt "1
     # Read all the CheckM output tables and filter genomes on their completeness and contamination scores 
     # according to the those provided in input
     touch ${TMPDIR}/genomes_qc.txt
-    for table in ${CHECKMTABLES//,/ }; do
+    CHECKMTABLES_ARR=($(echo $CHECKMTABLES | tr "," " "))
+    for table in ${CHECKMTABLES_ARR[@]}; do
         # Skip header line with sed
         # Discard genomes according to their completeness and contamination
         sed 1d $table | while read line; do
@@ -249,6 +269,12 @@ if [[ "${CHECKM_COMPLETENESS}" -gt "0" ]] && [[ "${CHECKM_CONTAMINATION}" -lt "1
 
     # Create a new INLIST file with the new list of genomes
     INLIST=${TMPDIR}/genomes_qc.txt
+    # Count how many genomes passed the quality control
+    HOW_MANY=$(cat ${INLIST} | wc -l)
+
+    printf "\t%s genomes passed the quality control\n" "${HOW_MANY}"
+    printf "\t\tMinimum completeness: %s\n" "${CHECKM_COMPLETENESS}"
+    printf "\t\tMaximum contamination: %s\n" "${CHECKM_CONTAMINATION}"
 fi
 
 # Use kmtricks to build a kmer matrix and compare input genomes
@@ -256,7 +282,7 @@ fi
 if ${DEREPLICATE}; then
     # Count how many input genomes survived in case they have been quality controlled
     HOW_MANY=$(cat ${INLIST} | wc -l)
-    if [[ "${HOW_MANY}" -gt "1" ]]; then
+    if [[ "${HOW_MANY}" -gt "1" ]]; then        
         GENOMES_FOF=${TMPDIR}/genomes.fof
         # Build a fof file with the list of input genomes
         while read -r genomepath; do
@@ -265,7 +291,7 @@ if ${DEREPLICATE}; then
         done <$INLIST
 
         if [[ -f ${GENOMES_FOF} ]]; then
-            printf "\tDereplicating %s input genomes" "${HOW_MANY}"
+            printf "\nDereplicating %s input genomes\n" "${HOW_MANY}"
             # Build matrix
             kmtricks pipeline --file ${GENOMES_FOF} \
                               --run-dir ${TMPDIR}/matrix \
@@ -280,6 +306,7 @@ if ${DEREPLICATE}; then
                                --cpr-in \
                                --sorted \
                                --threads ${NPROC} > ${TMPDIR}/kmers_matrix.txt
+            
             # Add header to the kmers matrix
             HEADER=$(awk 'BEGIN {ORS = " "} {print $1}' ${GENOMES_FOF})
             echo "#kmer $HEADER" > ${TMPDIR}/kmers_matrix_wh.txt
@@ -287,6 +314,7 @@ if ${DEREPLICATE}; then
             mv ${TMPDIR}/kmers_matrix_wh.txt ${TMPDIR}/kmers_matrix.txt
             # Remove duplicate columns
             cat ${TMPDIR}/kmers_matrix.txt | transpose | awk '!seen[substr($0, index($0, " "))]++' | transpose > ${TMPDIR}/kmers_matrix_dereplicated.txt
+            
             # Dereplicate genomes
             while read -r genome; do
                 if head -n1 ${TMPDIR}/kmers_matrix_dereplicated.txt | grep -q -w "$genome"; then
@@ -294,58 +322,188 @@ if ${DEREPLICATE}; then
                     realpath -s $(grep "^${genome} " ${TMPDIR}/genomes.fof | cut -d' ' -f3) >> ${TMPDIR}/genomes_dereplicated.txt
                 fi
             done <<<"$(head -n1 ${TMPDIR}/kmers_matrix.txt | tr " " "\n")"
+            
             # Use the new list of dereplicated genomes
             if [[ -f ${TMPDIR}/genomes_dereplicated.txt ]]; then
                 INLIST=${TMPDIR}/genomes_dereplicated.txt
             fi
+
+            # Count how many genomes passed the dereplication
+            HOW_MANY=$(cat ${INLIST} | wc -l)
+            printf "\t%s genomes passed the dereplication\n" "${HOW_MANY}"
         fi
     else
         # No input genomes survived from the quality control step
-        printf "\tNo input genomes available!\n"
+        printf "No input genomes available!\n"
+        PIPELINE_END_TIME="$(date +%s.%3N)"
+        PIPELINE_ELAPSED="$(bc <<< "${PIPELINE_END_TIME}-${PIPELINE_START_TIME}")"
+        printf "\nTotal elapsed time: %s\n\n" "$(displaytime ${PIPELINE_ELAPSED})"
+        
+        # Print credits before stopping the pipeline execution
+        credits
+        
         exit 0
     fi
 fi
 
-# Count how many input genomes survived in case they have been quality controlled
+# Count how many input genomes survived in case they have been quality controlled and dereplicated
 HOW_MANY=$(cat ${INLIST} | wc -l)
 if [[ "${HOW_MANY}" -gt "1" ]]; then
+    # Create a temporary folder in case input genomes are GZip compressed
+    mkdir -p $TMPDIR/genomes
+
+    # Init the list of taxonomies that must be rebuilt
+    REBUILD=()
     # Process input genomes
     while read GENOMEPATH; then
+        GENOMEEXT="${GENOMEPATH##*.}"
+        GENOMENAME="$(basename ${GENOMEPATH%.*})"
+        FILEPATH=$GENOMEPATH
         # Decompress input genome in case it is GZip compressed
-        # TODO
-        GENOMENAME="$(basename $GENOMEPATH)"
-        if [[ "$TYPE" = "MAGs" ]]; then
-            # Run the profiler to establish the closest group for each taxonomic level in the tree
-            . ${SCRIPT_DIR}/profile.sh --input=$GENOMEPATH \
-                                       --tree=$DBDIR/......./index/index.detbrief.sbt
-                                       --expand \
-                                       --output-dir=$TMPDIR/profiling/ \
-                                       --output-prefix=$GENOMENAME \
-                                       > /dev/null 2>&1 # Silence the profiler
-            # Define output file path with profiles
-            PROFILE=${TMPDIR}/profiling/${GENOMENAME}__profiles.txt
-            if [[ -f $PROFILE ]]; then
-                # If it is close enough to a group in a taxonomic level, remember that this genome must be assigned to the same group
-                # Otherwise, mark the genome as unassigned
-                #
-                # Assigned all the genomes to the closest groups and rebuild the SBTs
-                # Define new groups by looking at the kmer matrix of kmtricks for the unassigned genomes
-                #
-                # Discard the genome if the score is 1.0 compared to the closest match at the species level
-            fi
-        elif [[ "$TYPE" = "references" ]]; then
-            # If the taxonomic label of the input genome already exists in the database
-            #   Add the new genome to the assigned taxonomic branch and rebuild the SBTs
-            # Otherwise,
-            #   Compare the genome with genomes in newly defined clusters at the level of the known lineage
-            #   In case there is nothing close to the genome, create a new branch with the new taxonomy for that genome
-            #   Otherwise, merge the unknown clusters with the new genomes and assign the new taxonomy
-            #
+        if [[ "$GENOMEPATH" = *.gz ]]; then
+            GENOMEEXT="${GENOMENAME##*.}"
+            GENOMENAME="${GENOMENAME%.*}"
+            gunzip -c $GENOMEPATH > $TMPDIR/genomes/${GENOMENAME}.fna
+            FILEPATH=$TMPDIR/genomes/${GENOMENAME}.fna
+        fi
+
+        printf "\nProfiling %s\n" "$GENOMENAME"
+        # Run the profiler to establish the closest genome and the closest group for each taxonomic level in the tree
+        . ${SCRIPT_DIR}/profile.sh --input=$FILEPATH \
+                                   --tree=${DBDIR}/k__${KINGDOM}/index/index.detbrief.sbt
+                                   --expand \
+                                   --output-dir=$TMPDIR/profiling/ \
+                                   --output-prefix=$GENOMENAME \
+                                   > /dev/null 2>&1 # Silence the profiler
+        # Define output file path with profiles
+        PROFILE=${TMPDIR}/profiling/${GENOMENAME}__profiles.txt
+        
+        if [[ -f $PROFILE ]]; then
             # Discard the genome if the score is 1.0 compared to the closest match at the species level
+            # Do not discard the input genome if it is a reference genome and the closest genome in the database
+            # is a MAG with a perfect overlap of kmers (score 1.0)
+            CLOSEST_GENOME="$(grep "${GENOMENAME}.${GENOMEEXT}" $PROFILE | grep -w "genome" | cut -d$'\t' -f3)"
+            CLOSEST_GENOME_SCORE="$(grep "${GENOMENAME}.${GENOMEEXT}" $PROFILE | grep -w "genome" | cut -d$'\t' -f4)"
+            # Print closest genome
+            printf "\tClosest genome: %s (score: %s)\n" "${CLOSEST_GENOME}" "${CLOSEST_GENOME_SCORE}"
+            # Reconstruct the closest taxa
+            LEVELS=("kingdom" "phylum" "class" "order" "family" "genus" "species")
+            CLOSEST_TAXA=""
+            printf "\tClosest lineage:\n"
+            for level in ${LEVELS[@]}; do
+                CLOSEST_LEVEL="$(grep "${GENOMENAME}.${GENOMEEXT}" $PROFILE | grep -w "$level" | cut -d$'\t' -f3)"
+                CLOSEST_LEVEL_SCORE="$(grep "${GENOMENAME}.${GENOMEEXT}" $PROFILE | grep -w "$level" | cut -d$'\t' -f4)"
+                CLOSEST_TAXA=${CLOSEST_TAXA}"|"${CLOSEST_LEVEL}
+                # Print profiler result
+                printf "\t\t%s: %s (score: %s)\n" "${level}" "${CLOSEST_LEVEL}" "${CLOSEST_LEVEL_SCORE}"
+            done
+            # Trim the first pipe out of the closest taxonomic label
+            CLOSEST_TAXA=${CLOSEST_TAXA:1}
+            # Define taxa path
+            CLOSEST_TAXADIR=${DBDIR}/$(echo "${CLOSEST_TAXA}" | sed 's/|/\//g')
+
+            SKIP_GENOME=false
+            # Check whether the input genome must be discarded
+            if [[ "${CLOSEST_GENOME_SCORE}" -eq "1.0" ]]; then
+                if [[ "$TYPE" = "MAGs" ]]; then
+                    if [[ -f "${CLOSEST_TAXADIR}/genomes.txt" ]] && grep -q "" ${CLOSEST_TAXADIR}/genomes.txt; then
+                        # If the input genome is a MAG and the closest genome is a reference
+                        # Discard the input genome
+                        ${SKIP_GENOME}=true
+                        # Print the reason why we discard the input genome
+                        printf "\tDiscarding genome:\n"
+                        printf "\t\tInput genome is a MAG and the closest genome is a reference\n"
+                    elif [[ -f "${CLOSEST_TAXADIR}/mags.txt" ]] && grep -q "" ${CLOSEST_TAXADIR}/mags.txt; then
+                        # If the input genome is a MAG and the closest genome is a MAG
+                        # Discard the input genome
+                        ${SKIP_GENOME}=true
+                        # Print the reason why we discard the input genome
+                        printf "\tDiscarding genome:\n"
+                        printf "\t\tInput genome is a MAG and the closest genome is a MAG\n"
+                    fi
+                elif [[ "$TYPE" = "references" ]]; then
+                    if [[ -f "${CLOSEST_TAXADIR}/genomes.txt" ]] && grep -q "" ${CLOSEST_TAXADIR}/genomes.txt; then
+                        # If the input genome is a reference and the closest genome is a reference
+                        # Discard the input genome
+                        ${SKIP_GENOME}=true
+                        # Print the reason why we discard the input genome
+                        printf "\tDiscarding genome:\n"
+                        printf "\t\tInput genome is a reference and the closest genome is a reference\n"
+                    fi
+                fi
+            fi
+
+            if ! ${SKIP_GENOME}; then
+                # Process MAGs or references
+                if [[ "$TYPE" = "MAGs" ]]; then                
+                    # If it is close enough to a group in a taxonomic level, remember that this genome must be assigned to the same group
+                    # Otherwise, mark the genome as unassigned
+                    #
+                    # Assigned all the genomes to the closest groups and rebuild the SBTs
+                    # Define new groups by looking at the kmer matrix of kmtricks for the unassigned genomes
+                    # TODO
+                elif [[ "$TYPE" = "references" ]]; then
+                    # Retrieve the taxonomic label from --taxa input file
+                    TAXALABEL="$(grep -w "$GENOMENAME" $TAXA | cut -d$'\t' -f2)"
+                    # Define taxonomy folder
+                    TAXDIR=${DBDIR}/$(echo "${TAXALABEL}" | sed 's/|/\//g')
+                    TARGETGENOME=${TAXDIR}/genomes/${GENOMENAME}.${GENOMEEXT}
+                    # Print input genome taxonomic label
+                    printf "\tTaxonomic label:\n"
+                    printf "\t\t%s\n\n" "$TAXALABEL"
+
+                    if [[ -d $TAXDIR ]]; then
+                        # If the taxonomic label of the input genome already exists in the database
+                        # add the new genome to the assigned taxonomic branch and rebuild the SBTs
+                        cp $FILEPATH $TARGETGENOME
+                        if [[ ! "$GENOMEEXT" = "gz" ]]; then
+                            gzip ${TAXDIR}/genomes/${GENOMENAME}.${GENOMEEXT}
+                            TARGETGENOME=${TAXDIR}/genomes/${GENOMENAME}.${GENOMEEXT}.gz
+                        fi
+                        # Add the current genome to the list of genomes in current taxonomy
+                        echo "$TARGETGENOME" >> $TAXDIR/genomes.txt
+                        # Do not remove the index here because there could be other input genomes with the same taxonomic label
+                        REBUILD+=($TAXALABEL)
+                    else
+                        # Otherwise,
+                        #   Compare the genome with genomes in newly defined clusters at the level of the known lineage
+                        #   In case there is nothing close to the genome, create a new branch with the new taxonomy for that genome
+                        #   Otherwise, merge the unknown clusters with the new genomes and assign the new taxonomy
+                        # TODO
+                    fi
+                fi
+            fi
+        fi
+
+        # Remove the uncompressed genome in temporary folder
+        if [[ "$GENOMEPATH" = *.gz ]]; then 
+            rm $TMPDIR/genomes/${GENOMENAME}.fna
         fi
     done <$INLIST
+
+    # Extract taxonomic levels from the list of taxa that must be rebuilt
+    # Process all the species first, then all the genera, and so on up to the kingdom
+    for POS in $(seq 7 1); do 
+        TAXALIST=()
+        for LABEL in ${REBUILD[@]}; do
+            LEVELS=($(echo $LABEL | tr "|" " "))
+            TAXALIST+=($LEVELS[$POS])
+        done
+        # Remove duplicate entries
+        TAXALIST=$(printf "%s\0" "${TAXALIST[@]}" | sort -uz | xargs -0 printf "%s ")
+        # Rebuild the sequence bloom trees
+        # TODO
+    done
 else
-    printf "\tNo input genomes available!\n"
+    # No input genomes survived from the quality control step and dereplication
+    printf "No input genomes available!\n"
+    PIPELINE_END_TIME="$(date +%s.%3N)"
+    PIPELINE_ELAPSED="$(bc <<< "${PIPELINE_END_TIME}-${PIPELINE_START_TIME}")"
+    printf "\nTotal elapsed time: %s\n\n" "$(displaytime ${PIPELINE_ELAPSED})"
+    
+    # Print credits before stopping the pipeline execution
+    credits
+
     exit 0
 fi
 
