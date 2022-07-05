a33ab6d8a2d6378db2c906f0f3de3a41d2d7e614
fabio-cumbo <fabio.cumbo@gmail.com>
2022-07-05 17:50:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,26 +11,49 @@ setuptools.setup(name="MetaSBT",
                  url="http://github.com/cumbof/MetaSBT",
                  license="LICENSE",
                  packages=setuptools.find_packages(),
-                 package_data={
-                    "MetaSBT": [
-                        "requirements.txt",
-                        "LICENSE",
-                        "modules/*",
-                 ]},
+                 package_data = {
+                     "metasbt": [
+                        "requirements.txt"
+                     ],
+                 },
+                 scripts=[
+                     "scripts/esearch_txid.sh",
+                     "scripts/howdesbt_index.sh",
+                     "scripts/uniform_inputs.sh"
+                 ],
                  entry_points={
-                    "console_scripts": ["metasbt = metasbt:main"]
+                     "console_scripts": [
+                        "metasbt = metasbt.metasbt:main"
+                     ]
                  },
                  description=("A scalable framework for automatically indexing microbial genomes and accurately characterizing "
                               "metagenome-assembled genomes with sequence bloom trees"),
                  long_description=open("README.md").read(),
                  long_description_content_type="text/markdown",
                  install_requires=[
-                     "numpy==1.22.3",
-                     "tqdm==4.38.0"
+                     "ncbi-genome-download",
+                     "ncbitax2lin",
+                     "numpy",
+                     "requests",
+                     "tqdm"
                  ],
                  python_requires='>=3.8',
                  zip_safe=False,
                  classifiers=[
-                    "Programming Language :: Python :: 3",
-                    "License :: OSI Approved :: MIT License"
+                     "Environment :: Console",
+                     "Intended Audience :: Science/Research",
+                     "License :: OSI Approved :: MIT License",
+                     "Programming Language :: Python :: 3",
+                     "Topic :: Scientific/Engineering :: Bio-Informatics"
+                 ],
+                 keywords=[
+                     "bioinformatics",
+                     "characterization",
+                     "classification",
+                     "genomes",
+                     "metagenome-assembled genomes",
+                     "metagenomics",
+                     "microbiome",
+                     "profiling",
+                     "sequence bloom trees"
                  ])
