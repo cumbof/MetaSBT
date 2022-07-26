5b68b6cdb1b74db79477740c41227d76d0cd9051
fabio-cumbo <fabio.cumbo@gmail.com>
2022-07-26 12:03:44 -0400

    Updating setup.py
    Retrieve `__version__` from `metasbt`; add `download_url`; add `platforms`; add `project_urls`; update `classifiers`

--- setup.py
+++ setup.py
@@ -2,52 +2,39 @@ import sys
 
 import setuptools
 
+from metasbt.metasbt import __version__
+
 if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 8):
     sys.stdout.write(
-        "MetaSBT requires Python 3.8 or higher. Your Python your current Python version is {}.{}.{}".format(
+        "MetaSBT requires Python 3.8 or higher. Your Python your current Python version is {}.{}.{}\n".format(
             sys.version_info[0], sys.version_info[1], sys.version_info[2]
         )
     )
 
 setuptools.setup(
-    name="MetaSBT",
-    version="0.1.0a0",
     author="Fabio Cumbo",
     author_email="fabio.cumbo@gmail.com",
-    url="http://github.com/cumbof/MetaSBT",
-    license="MIT",
-    license_files=["LICENSE"],
-    packages=setuptools.find_packages(),
-    package_data={
-        "metasbt": ["requirements.txt"],
-    },
-    scripts=[
-        "scripts/esearch_txid.sh",
-        "scripts/howdesbt_index.sh",
-        "scripts/uniform_inputs.sh",
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
-    entry_points={"console_scripts": ["metasbt=metasbt.metasbt:main"]},
     description=(
         "A scalable framework for automatically indexing microbial genomes and accurately characterizing "
         "metagenome-assembled genomes with sequence bloom trees"
     ),
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
+    download_url="https://pypi.org/project/MetaSBT/",
+    entry_points={"console_scripts": ["metasbt=metasbt.metasbt:main"]},
     install_requires=[
         "ncbi-genome-download",
         "ncbitax2lin",
         "numpy",
         "requests",
         "tqdm",
-    ],
-    python_requires=">=3.8",
-    zip_safe=False,
-    classifiers=[
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
     keywords=[
         "bioinformatics",
@@ -60,4 +47,28 @@ setuptools.setup(
         "profiling",
         "sequence bloom trees",
     ],
+    license="MIT",
+    license_files=["LICENSE"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    name="MetaSBT",
+    packages=setuptools.find_packages(),
+    package_data={
+        "metasbt": ["requirements.txt"],
+    },
+    platforms=["Linux", "Mac OSX"],
+    project_urls={
+        "Discussions": "https://github.com/cumbof/MetaSBT/discussions",
+        "Issues": "https://github.com/cumbof/MetaSBT/issues",
+        "Source": "https://github.com/cumbof/MetaSBT",
+    },
+    python_requires=">=3.8",
+    scripts=[
+        "scripts/esearch_txid.sh",
+        "scripts/howdesbt_index.sh",
+        "scripts/uniform_inputs.sh",
+    ],
+    url="http://github.com/cumbof/MetaSBT",
+    version=__version__,
+    zip_safe=False,
 )
