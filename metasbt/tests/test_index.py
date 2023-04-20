542ca8e8fe2eaeb9c0c1d8b3c0a37d98aaf2f50b
fabio-cumbo <fabio.cumbo@gmail.com>
2023-04-20 18:01:29 -0400

    Updating unit tests for the index module

--- metasbt/tests/test_index.py
+++ metasbt/tests/test_index.py
@@ -8,10 +8,8 @@ __version__ = "0.1.0"
 __date__ = "Apr 1, 2023"
 
 import errno
-import hashlib
 import os
 import sys
-import tempfile
 import unittest
 
 # Define the module name
@@ -47,92 +45,6 @@ class TestUtils(unittest.TestCase):
     """
     Unit tests for index functions
     """
-
-    def test_download_assembly_summary(self):
-        """
-        Test the index.download_assembly_summary() function
-        Dependencies: wget
-        """
-
-        # Define the URL to the NCBI GenBank Assembly Summary report table
-        assembly_summary_url = "https://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_genbank.txt"
-
-        with tempfile.TemporaryDirectory() as tmpdir:
-            # Download and load the assembly summary table
-            assembly_table = index.download_assembly_summary(assembly_summary_url, tmpdir)
-
-            with self.subTest():
-                self.assertTrue(
-                    os.path.isfile(
-                        os.path.join(tmpdir, "{}.gz".format(os.path.basename(assembly_summary_url)))
-                    )
-                )
-
-        with self.subTest():
-            self.assertTrue(len(assembly_table) > 0)
-        
-    def test_download_taxdump(self):
-        """
-        Test the index.download_taxdump() function
-        Dependencies: wget
-        """
-
-        # Define the URL to the NCBI taxdump
-        taxdump_url = "https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz"
-
-        with tempfile.TemporaryDirectory() as tmpdir:
-            # Download nodes.dmp and names.dmp
-            nodes_dmp, names_dmp = index.download_taxdump(taxdump_url, tmpdir)
-
-            with self.subTest():
-                self.assertTrue(os.path.isfile(nodes_dmp))
-
-            with self.subTest():
-                self.assertTrue(os.path.isfile(names_dmp))
-
-    def test_load_taxa(self):
-        """
-        Test the index.load_taxa() function
-        Dependencies: ncbitax2lin, wget
-        """
-
-        # Define the URL to the NCBI taxdump
-        taxdump_url = "https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz"
-
-        with tempfile.TemporaryDirectory() as tmpdir:
-            # Download nodes.dmp and names.dmp
-            nodes_dmp, names_dmp = index.download_taxdump(taxdump_url, tmpdir)
-
-            with self.subTest():
-                self.assertTrue(os.path.isfile(nodes_dmp))
-
-            with self.subTest():
-                self.assertTrue(os.path.isfile(names_dmp))
-
-            # Build the ncbi_lineages table with ncbitax2lin
-            ncbitax2lin_table = index.ncbitax2lin(nodes_dmp, names_dmp, tmpdir)
-
-            with self.subTest():
-                self.assertTrue(os.path.isfile(ncbitax2lin_table))
-
-            tax_ids, tax_labels = index.load_taxa(
-                ncbitax2lin_table,
-                "Eukaryota",
-                "Fungi",
-                os.path.join(tmpdir, "taxamap.tsv")
-            )
-
-            with self.subTest():
-                self.assertTrue(len(tax_ids) > 0)
-
-            with self.subTest():
-                self.assertTrue(len(tax_labels) > 0)
-
-            with self.subTest():
-                self.assertEqual(len(tax_labels), len(tax_labels))
-
-            with self.subTest():
-                self.assertTrue(os.path.isfile(os.path.join(tmpdir, "taxamap.tsv")))
 
     def test_get_sublist(self):
         """
