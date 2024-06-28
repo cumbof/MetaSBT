d8989ec27b820839482017b32add473d7c32627e
Fabio Cumbo <fabio.cumbo@gmail.com>
2024-06-28 05:24:01 -0400

    Update __date__

--- metasbt/metadata.py
+++ metasbt/metadata.py
@@ -5,7 +5,7 @@ __author__ = "Fabio Cumbo, Daniel Blankenberg"
 __copyright__ = "Copyright 2022, Blankenberg Lab"
 __credits__ = ["Fabio Cumbo", "Daniel Blankenberg"]
 
-__date__ = "Apr 12, 2024"
+__date__ = "Jun 28, 2024"
 __email__ = "fabio.cumbo@gmail.com"
 __license__ = "MIT"
 __maintainer__ = "Fabio Cumbo"
