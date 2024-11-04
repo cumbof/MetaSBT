d7f490e184e5cd9b8135b374ca55bb1286194d23
Fabio Cumbo <fabio.cumbo@gmail.com>
2024-11-04 13:05:44 -0500

    Bump versiont to v0.1.4

--- metasbt/metadata.py
+++ metasbt/metadata.py
@@ -5,9 +5,9 @@ __author__ = "Fabio Cumbo, Daniel Blankenberg"
 __copyright__ = "Copyright 2022, Blankenberg Lab"
 __credits__ = ["Fabio Cumbo", "Daniel Blankenberg"]
 
-__date__ = "Jun 28, 2024"
+__date__ = "Oct 11, 2024"
 __email__ = "fabio.cumbo@gmail.com"
 __license__ = "MIT"
 __maintainer__ = "Fabio Cumbo"
 __status__ = "Production"
-__version__ = "0.1.3"
+__version__ = "0.1.4"
