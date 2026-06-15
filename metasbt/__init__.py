9aaa6e18694343ef2af6a78ea614ca567773bc0c
Fabio Cumbo <fabio.cumbo@gmail.com>
2026-06-14 21:14:01 -0400

    Bump MetaSBT version to v2.0.0

--- metasbt/__init__.py
+++ metasbt/__init__.py
@@ -6,5 +6,5 @@ modules and how to use the framework and its databases."""
 
 __author__ = ("Fabio Cumbo (fabio.cumbo@gmail.com)")
 
-__version__ = "0.1.6"
-__date__ = "May 29, 2026"
+__version__ = "2.0.0"
+__date__ = "June 14, 2026"
